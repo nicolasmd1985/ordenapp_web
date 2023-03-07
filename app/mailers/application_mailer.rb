391853cea9f26bc0bf62cc0839aaae54aaaa9666
NicolasM <nmahecha@hanekedesign.com>
2023-03-07 16:10:03 -0500

    update sender mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
   # add_template_helper(EmailHelper)
-  default from: 'Ordenapp <contacto@ordenapp.co>'
+  default from: 'Ordenapp <nicolas.mahecha@dipzo.net>'
   layout 'mailer'
 
   def demo_request_mail(name, email, phone ,accept_terms )
@@ -8,6 +8,6 @@ class ApplicationMailer < ActionMailer::Base
     @email = email
     @phone = phone
     @accept_terms = accept_terms
-    mail(to: 'Mercadeo <mercadeo@ordenapp.co>', from:'Ordenapp <contacto@ordenapp.co>', subject: "#{@name} is requesting a demo")
+    mail(to: 'Mercadeo <mercadeo@ordenapp.co>', from:'Ordenapp <nicolas.mahecha@dipzo.net>', subject: "#{@name} is requesting a demo")
   end
 end
