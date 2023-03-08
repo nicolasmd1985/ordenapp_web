ab2a917b74f8eeae06b592391816544845f980cd
NicolasM <nmahecha@hanekedesign.com>
2023-03-08 17:35:26 -0500

    fix parsing utf8

--- app/services/ensure_utf8_encode.rb
+++ app/services/ensure_utf8_encode.rb
@@ -5,7 +5,7 @@ class EnsureUtf8Encode
   end
 
   def process
-    @string = @string.encode(Encoding.find('UTF-8'), { invalid: :replace, undef: :replace, replace: '' })
+    @string = @string.scrub
     @string = @string.each_char.select{|c| c.bytes.first < 240 }.join('')
     return @string
   end
