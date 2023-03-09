2a06a0b93cbac9a7440ca4fb873c080151bfd1a7
NicolasM <nmahecha@hanekedesign.com>
2023-03-09 12:09:20 -0500

    fix push

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,8 +4,8 @@ CarrierWave.configure do |config|
     provider:              'AWS',                        # required
     aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],                        # required unless using use_iam_profile
     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],                        # required unless using use_iam_profile
-    use_iam_profile:       true                            # optional, defaults to false
-    region:                ENV['AWS_REGION'],                  # optional, defaults to 'us-east-1'
+    use_iam_profile:       true,                            # optional, defaults to false
+    region:                ENV['AWS_REGION']                  # optional, defaults to 'us-east-1'
     # host:                  's3.example.com',             # optional, defaults to nil
     # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
   }
