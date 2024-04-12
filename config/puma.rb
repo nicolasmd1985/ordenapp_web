575fb12cd8cb955b6660ded4b872a8343cd83e86
Nicolas Mahecha <nicolasmelectronic@gmail.com>
2024-04-12 13:23:25 -0400

    change puma

--- config/puma.rb
+++ config/puma.rb
@@ -7,9 +7,11 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 3 }
 threads threads_count, threads_count
 
+bind "unix:///var/run/puma/my_app.sock?umask=0000"
+
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port        ENV.fetch("PORT") { 3000 }
+# port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
