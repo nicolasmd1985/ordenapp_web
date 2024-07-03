dc8e3b3e51278d0d33976e2b96ac591a7c726470
Nicolas Mahecha <nicolasmelectronic@gmail.com>
2024-07-03 17:25:34 -0400

    change config docker

---
+++ deploy_script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#if docker-compose is runing
+ENV=production docker-compose down
+
+# Pull the latest images
+ENV=production docker-compose pull
+
+# Run migrations
+ENV=production docker-compose run --rm web rails db:create db:migrate
+
+# Seed the database
+ENV=production docker-compose run --rm web rails db:seed
+
+# Start the containers in detached mode
+ENV=production docker-compose up -d
