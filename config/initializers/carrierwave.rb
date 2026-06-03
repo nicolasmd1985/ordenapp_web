16cb0b5b49d0263bd40f6cdca8f7b96db2a2da4e
Nicolas Mahecha <nicolasmelectronic@gmail.com>
2026-06-03 19:56:22 -0400

    feat: configure CarrierWave S3 endpoint for GCP Cloud Storage compatibility

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,3 @@
-
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:              'AWS',                        # required
@@ -6,9 +5,14 @@ CarrierWave.configure do |config|
     aws_secret_access_key: ENV['aws_secret_access_key'],                        # required unless using use_iam_profile
     use_iam_profile:       false,                            # optional, defaults to false 
     region:                ENV['AWS_REGION']                 # optional, defaults to 'us-east-1'
-    # host:                  's3.example.com',             # optional, defaults to nil
-    # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
   }
+
+  # Use custom endpoint for Google Cloud Storage S3 Interoperability API if present
+  if ENV['S3_ENDPOINT'].present?
+    config.fog_credentials[:endpoint] = ENV['S3_ENDPOINT']
+    config.fog_credentials[:path_style] = true
+  end
+
   config.fog_directory  = ENV['S3_BUCKET_NAME']                                      # required
   config.fog_public    = true
   config.cache_dir     = "#{Rails.root}/tmp/uploads"         # To let CarrierWave work on Heroku
